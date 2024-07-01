cf8d040d13e0915473783c1f304e30dcbe90acac
Malcolm LaRose <malcolmflarose@gmail.com>
2024-07-01 12:57:09 -0400

    fast to string implemented

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 // Goals for AFTER completion --> don't touch until GOL works first, do roughly in order
 // Framerate display optimization
 // Clean up/ refactor code to make more modular/extendable
+// GUI
 // Blue cell under cursor --> DONE
+// Drawing tools (lines, etc.)
 // more optimization...
 // Menus, controls, etc...
 // Options
-// GUI
 // More than one type of life --> look at examples
 
 
@@ -23,6 +24,54 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+
+
+
+
+const char digit_pairs[201] = {
+  "00010203040506070809"
+  "10111213141516171819"
+  "20212223242526272829"
+  "30313233343536373839"
+  "40414243444546474849"
+  "50515253545556575859"
+  "60616263646566676869"
+  "70717273747576777879"
+  "80818283848586878889"
+  "90919293949596979899"
+};
+
+
+// Algorithm provided by user TIMO on SO: https://stackoverflow.com/questions/4351371/c-performance-challenge-integer-to-stdstring-conversion
+
+static const int BUFFER_SIZE = 11;
+
+char buf[BUFFER_SIZE]; // My modification
+
+std::string itostr(unsigned int val)
+{
+
+	char* it = (char*)&buf[BUFFER_SIZE - 2];
+
+	int div = val / 100;
+	while (div) {
+		memcpy(it, &digit_pairs[2 * (val - div * 100)], 2);
+		val = div;
+		it -= 2;
+		div = val / 100;
+	}
+	memcpy(it, &digit_pairs[2 * val], 2);
+	if (val < 10)
+		it++;
+
+	return std::string((char*)it, (char*)&buf[BUFFER_SIZE] - (char*)it);
+}
+
+
+
+
+
 
 
 GoL_Settings& gols = GoL_Settings::getSettings();
@@ -200,6 +249,7 @@ public:
 		window.create(sf::VideoMode(gols.initScreenWidth, gols.initScreenHeight), "Physics Box", sf::Style::Titlebar | sf::Style::Close);
 		window.setKeyRepeatEnabled(false);
 		window.setVerticalSyncEnabled(false); // Probably ignored by driver
+		window.setMouseCursorVisible(false);
 		// window.setFramerateLimit(targetFPS);
 
 
@@ -256,7 +306,7 @@ public:
 
 
 	void frameCounterDisplay(const int& frameTime, const int& avg) {
-		frameText.setString("F-Time (us): " + std::to_string(frameTime) + "\nAvg FPS: " + std::to_string(avg));
+		frameText.setString("F-Time (us): " + itostr(frameTime) + "\nAvg FPS: " + itostr(avg));
 
 		window.draw(frameText);
 	}
@@ -281,10 +331,10 @@ public:
 				if (!paused) {
 					update();
 				}
-				frameCounterDisplay(frameTime, frameCount / (totalFrameTime / 1000000));
 				timer -= delay;
 			}
 			renderAll();
+			frameCounterDisplay(frameTime, frameCount / (totalFrameTime / 1000000));
 			frameCount++;
 			window.display();
 
